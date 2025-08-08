165a9caf4cdc842c370e3bf2631278dcbbc58bbc
kanetaku1 <ublvsp010111@gmail.com>
2025-08-09 03:07:14 +0900

    gitignoreを編集

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    authInterrupts: true,
+  },
 };
 
 export default nextConfig;
