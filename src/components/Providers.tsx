c816b5631899438b95fffa3ada4c2d8d33b240dc
kanetaku1 <ublvsp010111@gmail.com>
2025-11-28 16:32:25 +0900

    refactor: コンポーネントの更新
    - Providersコンポーネントの更新
    - その他のコンポーネントの調整

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,15 +1,9 @@
 'use client'
 
-import { ReactNode, useEffect } from 'react'
+import { ReactNode } from 'react'
 import { ToastProvider } from '@/components/ui/use-toast'
-import { initializeDemoData } from '@/lib/demoData'
 
 export function Providers({ children }: { children: ReactNode }) {
-  useEffect(() => {
-    // デモデータを初期化
-    initializeDemoData()
-  }, [])
-
   return (
     <ToastProvider>
       {children}
